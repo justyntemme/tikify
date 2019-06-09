8e992f15036d8d4428e2538848ccf1f1fc3239fb
Justyn Temme <j_temme@apple.com>
2019-06-09 11:05:25 -0500

    adding error message

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,8 +1,14 @@
 <template>
     <div class="sign-up">
         <p>Create a new account</p>
+        <b-alert
+            :show="dismissCountDown"
+            dismissible
+            variant="warning"
+            @dismissed="dismissCountDown=0"
+            @dismiss-count-down="countDownChanged"> {{errorMsg}}</b-alert>
+        
         <input type="text" v-model="email" placeholder="Email"><br>
-        <input type="text" placeholder="Username"><br>
         <input type="password" v-model="password" placeholder="Password"><br>
         <button @click="signUp">Sign Up</button>
     </div>
@@ -18,17 +24,30 @@ import firebase from 'firebase'
             return {
                 email: '',
                 password: '',
+                dismissSecs: 5,
+                dismissCountDown: 0,
+                errorMsg: ''
             }
         },
         methods: {
+            countDownChanged(dismissCountDown) {
+                let v = this;
+                v.dismissCountDown = dismissCountDown
+            },
+            showAlert() {
+                let v = this;
+                v.dismissCountDown = v.dismissSecs
+            },
             signUp: function() {
-                
+                let v = this;
                 firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(
                     function(user){
                         alert('Your account has been created ' + user.email)
                     },
                     function (err) {
-                        alert('oops ' + err.message)
+                         v.errorMsg = err.message;
+                         v.showAlert();
+                        
                     }
                 )
             }
