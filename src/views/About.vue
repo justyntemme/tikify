1b59fd4ef08608b4b2dbb5a90efd8aad216338e1
Justyn Temme <justyntemme@gmail.com>
2019-06-18 14:04:47 -0500

    Home page template

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,42 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <p>Login <a href="/login">Here</a></p>
+    <div class="home-header row">
+      <div class="col-6 home-logo">
+        <img class="nav-logo" src="@/assets/logo.png"/>
+      </div>
+      <div class="ml-4 col-4 pl-5 home-login">
+        <router-link :to="{ name: 'login' }"><b-button class="home-nav-btn" pill variant="primary">Login</b-button></router-link>
+      </div>
+    </div>
+    <div class="welcome ">
+      <h1 class="get-started">Get Started!</h1>
+    </div>
+    <div class="row">
+      <div class='col-4'>
+        <b-button class="home-nav-btn" pill variant="primary">I'm an Influencer</b-button>
+      </div>
+
+    <div class="col-4 phone">
+      <img class="phone-img" src="@/assets/phone.jpeg"/>
+    </div>
+     <div class='col-4'>
+       <b-button class="home-nav-btn" pill variant="primary">I'm an Artist</b-button>
+      </div>
+
+    </div>
+
+    <div class="footer">
+      <img class="bottom-border" src="@/assets/bottom-border.jpeg"/>
+      </div>
   </div>
 </template>
+
+<style scoped>
+.home-header {
+  background-image: url('~@/assets/home-header.jpeg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+</style>
