ecb62e65263a7deb719a4693c415f9f39e1e07eb
Justyn Temme <j_temme@apple.com>
2019-06-21 09:05:14 -0500

    Fix css

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -26,5 +26,49 @@ export default {
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="css">
+a {
+  color: #E91E63;
+}
+input {
+  background: #F5F5F5; 
+  border: 1px solid #FFFFFF; 
+  border-radius: 20px; 
+  margin: 5px;
+  line-height: 2em;
+}
+.b-row {
+  width: 100%;
+  padding: 0px;
+  margin: 0px;
+}
+.home-logo {
+  height: 40px;
+  width: 90px;
+}
+.bottom-border {
+  width: 100%;
+}
+h1 {
+  font-family: 'Montserrat', sans-serif;
+}
+h2 {
+  font-family: 'Montserrat', sans-serif;
+}
+.about-flow h2 {
+  top: 40%;
+}
+
+.login-img {
+  height: 50px;
+  width: 50px;
+}
+
+
+.nav-logo {
+  margin-top: 5px;
+  height: 40px;
+  width: 90px;
+}
+
 
 </style>
