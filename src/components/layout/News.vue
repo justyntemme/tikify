50405dbf3b1877cc104016e84dbe73d91c86dcc5
Justyn Temme <justyntemme@gmail.com>
2019-06-21 02:10:44 -0500

    Chante styling and news section

--- src/components/layout/News.vue
+++ src/components/layout/News.vue
@@ -4,32 +4,20 @@
         <b-row>
             <b-col class="news-box">
                 <div class="news-body">
-                <h3>News Header</h3>
+                <h3>News 1</h3>
                 <p>The latest update of tikify brings us feature such as ...</p>
                 </div>
             </b-col>
             <b-col class="news-box">
                 <div class="news-body">
-                <h3>News Header</h3>
+                <h3>News 2</h3>
                 <p>The latest update of tikify brings us feature such as ...</p>
                 </div>
             </b-col>
         </b-row>
         <h2>Sales and statistics</h2>
         <b-row>
-            <b-col class="news-box">
-                <div class="news-body">
-                <h3>News Header</h3>
-                <p>The latest update of tikify brings us feature such as ...</p>
-                </div>
 
-            </b-col>
-            <b-col class="news-box">
-                <div class="news-body">
-                <h3>News Header</h3>
-                <p>The latest update of tikify brings us feature such as ...</p>
-                </div>
-            </b-col>
 
         </b-row>
     </b-container>
@@ -50,9 +38,11 @@ export default {
     background-size: cover;
     background-repeat: no-repeat;
     border-radius: 40px;
+
 }
 
 .news-body {
     margin-top: 40px;
+    
 }
 </style>
--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -2,7 +2,7 @@
 <b-container>
     <h3 class="product-title ">Products ></h3>
     <b-row class="b-row col-12 align-items-center">
-        <b-col  v-for="product in products" class="col-4 justify-content-center">
+        <b-col  v-for="product in products" class="col-6 justify-content-center">
             <router-link :to="{ name: 'product', params: { product: product}}"><b-card class="product-card">
             <b-card-title class="card-title">{{product.type}}</b-card-title>
             <b-card-text class="card-text">{{product.price}}</b-card-text>
