df47f2c37f889c067e22094d7aae960dc14726d6
Justyn Temme <justyntemme@gmail.com>
2019-06-20 23:57:41 -0500

    Formatting

--- src/components/layout/FooterMenu.vue
+++ src/components/layout/FooterMenu.vue
@@ -2,17 +2,15 @@
     <div class="footer">
         <b-container>
             <b-row>
-                <b-col>
+                <b-col class="footer-menu-col">
                     <router-link :to="{ name: 'home'}"> <b-button class="" variant="secondary">⌂</b-button></router-link>
                 </b-col>
                 <b-col>
                      <router-link :to="{ name: 'productcreate'}"> <b-button class="" variant="secondary">+</b-button></router-link>
                 </b-col>
-
                 <b-col>
                     <router-link :to="{ name: 'products'}"> <b-button class="" variant="secondary">Posts</b-button></router-link>
                 </b-col>
-
                 <b-col>
                     <router-link :to="{ name: 'sales'}"> <b-button class="" variant="secondary">$</b-button></router-link>
                 </b-col>
