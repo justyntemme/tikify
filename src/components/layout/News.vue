1eb6eae17d70fac22083c7f6b1ed7f7c68c0f3f3
Justyn Temme <j_temme@apple.com>
2019-06-21 01:04:14 -0500

    Home styling

--- src/components/layout/News.vue
+++ src/components/layout/News.vue
@@ -2,21 +2,21 @@
     <b-container class="news">
         <h2>Top News</h2>
         <b-row>
-            <b-col>
+            <b-col class="about-box">
                 <b-card>
                     <b-card-header>First News Header </b-card-header>
                     <b-card-body>Lorm ipsum inded general audios</b-card-body>
                     <b-card-img src="~@/assets/logo.png"></b-card-img>
                 </b-card>
             </b-col>
-            <b-col>
+            <b-col class="about-box">
                 <b-card>
                     <b-card-header>First News Header </b-card-header>
                     <b-card-body>Lorm ipsum inded general audios</b-card-body>
                     <b-card-img src="~@/assets/logo.png"></b-card-img>
                 </b-card>
             </b-col>
-            <b-col>
+            <b-col class="about-box">
                 <b-card>
                     <b-card-header>First News Header </b-card-header>
                     <b-card-body>Lorm ipsum inded general audios</b-card-body>
@@ -26,19 +26,19 @@
         </b-row>
         <h2>Sales and statistics</h2>
         <b-row>
-            <b-col>
-                <b-card>
+            <b-col class="about-box">
+                <b-card class="about-box">
                     <b-card-header>Total Sales </b-card-header>
                     <b-card-body>Lorm ipsum inded general audios</b-card-body>
                 </b-card>
             </b-col>
-            <b-col>
+            <b-col class="about-box">
                 <b-card>
                     <b-card-header>Total Sales This Month</b-card-header>
                     <b-card-body>Lorm ipsum inded general audios</b-card-body>
                 </b-card>
             </b-col>
-            <b-col>
+            <b-col class="about-box">
                 <b-card>
                     <b-card-header>Open Orders </b-card-header>
                     <b-card-body>Lorm ipsum inded general audios</b-card-body>
