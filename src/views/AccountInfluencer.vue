c97281bd4bba495cbe158047f4e503917847ca3b
Justyn Temme <justyntemme@gmail.com>
2019-07-25 23:42:28 -0500

    Add account page to change bio and reset passwrd

--- src/views/AccountInfluencer.vue
+++ src/views/AccountInfluencer.vue
@@ -3,15 +3,54 @@
         <b-container>
             <b-row>
                 <b-col class="content-col">
-                    <div class="col-12 justify-content-center">
-                        <img src="@/assets/add-image.svg">
-                            <b-form-textarea
-                            id="bio"
-                            v-model="account.bio"
-                            placeholder="Your Bio, tell us about yourself!"
-                            rows="10"
-                            max-rows="50"
-                            ></b-form-textarea>
+                    <div class="col-12">
+                        <b-row>
+                            <b-col class="justify-content-center">
+                                <img src="@/assets/add-image.svg">
+                            </b-col>
+                        </b-row>
+                        <b-row>
+                            <b-col class="col-4 justify-content-left">
+                                <h3 class="">Bio</h3>
+                            </b-col>
+                        </b-row>
+                        <b-row>
+                            <b-col>
+                                <b-form-textarea
+                                id="bio"
+                                v-model="account.bio"
+                                placeholder="Your Bio, tell us about yourself!"
+                                rows="10"
+                                max-rows="50"
+                                ></b-form-textarea>
+                            </b-col>
+                        </b-row>
+                        <b-row>
+                            <b-col class="col-4">
+                                <h3 class="">Email</h3>
+                            </b-col>
+                        </b-row>
+                        <b-row>
+                            <b-col>
+                                <p>{{account.email}}</p>
+                            </b-col>
+                        </b-row>
+                        <b-row>
+                            <b-col class="col-4">
+                                <h3 class="">Name</h3>
+                            </b-col>
+                        </b-row>
+                        <b-row>
+                            <b-col>
+                                <p>{{account.name}}</p>
+                            </b-col>
+                        </b-row>    
+                        <b-row>
+                            <b-col>
+                                <a @click="resetPassword" href="#">Reset Password</a>
+                            </b-col>
+                        </b-row>     
+                    
                     </div>
                 </b-col>
             </b-row>
@@ -50,6 +89,17 @@ export default {
 
   },
   methods: {
+      resetPassword(){
+          let v = this;
+          var auth = firebase.auth();
+          var emailAddress = v.account.email
+
+          auth.sendPasswordResetEmail(emailAddress).then(function(){
+              alert("Reset Email Sent")
+          }).catch(function(error) {
+              console.log(error)
+          })
+      }
 
   }
 }
@@ -60,6 +110,11 @@ export default {
 <style scoped>
 .content-col {
     margin-top: 20px;
+}
+
+h3 {
+    text-align: left;
+    margin: 5px;
 }
 
 </style>
